481381cfe84d4e2408c95f48fe2d7b22cd4daff2
Ese <eomene2@uic.edu>
2021-04-14 20:15:58 -0500

    Update shadowFrag.glsl.js

--- shadowFrag.glsl.js
+++ shadowFrag.glsl.js
@@ -23,15 +23,11 @@ void main() {
     float currentDepth = projCoords.z;
     float shadow = 0.0;
     vec2 texelSize = 1.0 / textureSize(projCoords, 0);
+    bias = 0.0025;
     for(int x = -1; x <= 1; ++x){
         for(int y = -1; y <= 1; ++y){
             float pcfDepth = texture(projCoords, vLightSpacePos.xy + vec2(x, y) * texelSize).r;
-            if(currentDepth > closestDepth){
-                shadow = 1.0;
-            }
-            else{
-                shadow = 0.0;
-            }
+            shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;
         }
     }
     shadow /= 9.0;
